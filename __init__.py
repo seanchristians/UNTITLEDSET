6af4777ce0d1e7bf65ddda0937bde4fff0c47b7d
Sean Christians <=>
2020-05-10 18:32:31 -0400

    Write user code

--- __init__.py
+++ __init__.py
@@ -1,8 +1,19 @@
 from eponym import cache, extract
+import random, sys
 
-class Synonyms:
-	def __init__(self, sentence):
-		self.sentence = sentence
-		self.syn = []
-		for base in sentence.split(' '):
-			self.syn += cache.get(base)
+def synonyms(sentence=''):
+	sentence = sentence.split(' ') if len(sentence) > 0 else ''
+	syn = []
+	for base in sentence:
+		syn += cache.get(base)
+	return syn
+
+def rand(syn, *, words=1, sep='', upper=False, maxWordLength=sys.maxsize):
+	var = ['']
+	for i in range(words):
+		c = ''
+		while c in var or len(c) > maxWordLength:
+			c = random.choice(syn)
+
+		var.append(c.upper() if upper else c)
+	return sep.join(var)[len(sep):]
