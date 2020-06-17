13c2dafe70cd59108f05bddf79c318a66de04fc5
Sean Christians <seanchristians.scc@gmail.com>
2020-06-17 14:21:23 -0400

    Fix upload/runtime issues on pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
 	name = "eponym-alloy",
-	version = "1.0.0",
+	version = "1.0.1",
 	author = "Sean Christians",
 	author_email = "seanchristians.scc@gmail.com",
 	description = "Useful tool for generating variable names",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	url = "https://github.com/seanchristians/eponym",
-	packages = setuptools.find_packages(),
+	packages = ["eponym"],
+	package_dir = {
+		"eponym": "src"
+	},
 	classifiers = [
 		"License :: OSI Approved :: GNU Affero General Public License v3",
 		"Natural Language :: English",
@@ -20,5 +23,11 @@ setuptools.setup(
 		"Programming Language :: Python :: 3.8",
 		"Topic :: Database"
 	],
+	install_requires = ["requests"],
+	entry_points = {
+		"console_scripts": [
+			"eponym=eponym.__main__:main"
+		]
+	},
 	python_requires = ">=3.8"
 )
--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,21 @@
 import argparse, sys
 import eponym
 
-parser = argparse.ArgumentParser(description="Variable name generator")
-parser.add_argument("words", nargs="+", help="list of input words")
-parser.add_argument("-k", "--key", default=eponym.interface.APIKey.env, help="api key")
-parser.add_argument("-l", "--lower", action="store_false", default=True, help="output lowercase")
-parser.add_argument("-w", "--words", dest='w', action="count", default=1, help="output word count")
-parser.add_argument("-s", "--sep", default='', help="word separator")
-parser.add_argument("-m", "--max", default=sys.maxsize, type=int, help="maximum word length for each synonym")
-args = parser.parse_args()
+def main():
 
-key = eponym.interface.APIKey(args.key)
+	parser = argparse.ArgumentParser(description="Variable name generator")
+	parser.add_argument("words", nargs="+", help="list of input words")
+	parser.add_argument("-k", "--key", default=eponym.interface.APIKey.env, help="api key")
+	parser.add_argument("-l", "--lower", action="store_false", default=True, help="output lowercase")
+	parser.add_argument("-w", "--words", dest='w', action="count", default=1, help="output word count")
+	parser.add_argument("-s", "--sep", default='', help="word separator")
+	parser.add_argument("-m", "--max", default=sys.maxsize, type=int, help="maximum word length for each synonym")
+	args = parser.parse_args()
 
-syn = eponym.synonyms(key, ' '.join(args.words))
-print(eponym.rand(syn, words=args.w, sep=args.sep, upper=args.lower, maxWordLength=args.max))
+	key = eponym.interface.APIKey(args.key)
+
+	syn = eponym.synonyms(key, ' '.join(args.words))
+	print(eponym.rand(syn, words=args.w, sep=args.sep, upper=args.lower, maxWordLength=args.max))
+
+if __name__ == "__main__":
+	main()
